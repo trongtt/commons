fc6217ba59a6a8ae4ff5078c268bf9d744d39df1
Thomas Delhoménie <tdelhomenie@exoplatform.com>
2016-08-31 22:48:46 +0200

    Revert "COMMONS-497 : call super setup in ElasticSearchingIntegrationTest to correctly initialize everything for these tests"
    This reverts commit 205f3aa56f1eee109e9ad5283f0fbe5859da6f06.

--- commons-search/src/test/java/org/exoplatform/addons/es/integration/ElasticSearchingIntegrationTest.java
+++ commons-search/src/test/java/org/exoplatform/addons/es/integration/ElasticSearchingIntegrationTest.java
@@ -43,8 +43,6 @@ public class ElasticSearchingIntegrationTest extends BaseIntegrationTest {
 
   @Before
   public void initServices() {
-    super.setup();
-
     Identity identity = new Identity("BCH", Collections.singletonList(new MembershipEntry("Admin")));
     ConversationState.setCurrent(new ConversationState(identity));
     elasticSearchServiceConnector = new ElasticSearchServiceConnector(getInitConnectorParams(), elasticSearchingClient);
